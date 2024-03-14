fdf251fc89829fceafbf20642653c49a34639772
Xeli Vargas <xeli.vargas@uteca.edu.mx>
2024-03-14 08:33:03 -0600

    :(
    )

--- libs/classes.py
+++ libs/classes.py
@@ -1,10 +1,25 @@
-class vertice():
-    def __init__(self,valor) -> None:
-        self.valor= valor 
+from .functions import *
+class grafo ():
+        
+   def __init__(self ) -> None :
+        self.Arista = {}
         pass
-pass
+   def addVertice (self,vertice):
+        self.Arista[vertice]={}
+        pass 
+   def addArista(self,origen ,destino,peso):
+        if origen not in self.Arista:
+            self.addVertice(origen )
+        if destino not in self.Arista:
+            self.addVertice(destino)
+        self.addArista[origen].update({destino:peso})
 
-class grafo (arista):
-    def __init__(self , origen , destino , peso ) -> None :
-        super().__init__(origen ,destino,peso)
+   def __str__(self) -> str:
+        return printDicc(self.Arista)
+    
+
+
+
+        
+
         pass
--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 from libs import *
-import sys
-
-#dicc = {
- #   'nom' :'xeli',
-#    'edad':18
-#    'tel':{
-#        'cel':5,
- #       'fij':6
- #   }
-           
-#}
-
-#print (dicc["edad"])
-
-diccRel={
+"""diccRel={
     'A':'B':5,'C':3
     'B':'A':5,'C':2,'D':4
     'C':'A':3,'B':2,'D':6,'E':7
@@ -23,12 +9,39 @@ diccRel={
 #diccRel={'A':'B'}
 #diccRel={'B':'D'}
 #diccRel.update({'C':'D'})
+
 diccRel['A']={'B':5,'C':3}
 diccRel['B']={'A':5,'C':2,'D':4}
 diccRel['C']={'A':3,'B':2,'D':6,'E':7}
 diccRel['D']={'B':4,'C':6,'E':8}
 diccRel['E']={'C':7,'D':8}
 
-printDicc(diccRel)
-verticeA = vertice ('A')
-arista1=arista ('A':'B')
+printDicc(diccRel)"""
+
+grafoTest=grafo()
+
+grafoTest.addArista('A','B',5)
+grafoTest.addArista('A','C',3)
+
+grafoTest.addArista('B','A',5)
+grafoTest.addArista('B','D',4)
+grafoTest.addArista('B','C',2)
+
+grafoTest.addArista('C','A',3)
+grafoTest.addArista('C','B',2)
+grafoTest.addArista('C','D',6)
+
+grafoTest.addArista('D','C',7)
+grafoTest.addArista('D','B',4)
+grafoTest.addArista('D','E',8)
+
+grafoTest.addArista('E','D',7)
+grafoTest.addArista('E','C',8)
+origenG ='A'
+destinoG='E'
+path={}
+path['-']=0
+print(grafoTest.Arista['A']['B'])
+print(grafoTest.Arista[origenG])
+print (path)
+print(grafoTest)
